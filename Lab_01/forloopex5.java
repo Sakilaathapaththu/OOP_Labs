45d8f8f8fe98a2c28c3c8940564e7479fcba2c16
Athapaththu <99331500+Sakilaathapaththu@users.noreply.github.com>
2022-09-20 11:17:13 +0530

    Add files via upload

---
+++ Lab_01/forloopex5.java
@@ -0,0 +1,19 @@
+
+public class forloopex5 {
+	
+	public static void main (String[] args) {
+		for(int a=0;a<5;a++)
+		{
+			for(int s=0;s<5-a;s++){
+				System.out.print(" ");
+			}
+			for(int v=0;v<=a;v++){
+				
+				System.out.print(" * ");
+			}
+			System.out.print("\n");
+			
+		}
+	}
+}
+
